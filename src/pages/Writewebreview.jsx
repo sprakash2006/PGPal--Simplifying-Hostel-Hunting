66a87f8e94ed9bae0c0e872454c28ec44060698b
praka <prakashswami150569@gmail.com>
2025-09-06 00:52:41 +0530

    added write a review page

---
+++ src/pages/Writewebreview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Writewebreview = () => {
+  return (
+    <div>
+      <h1>Here the form of writing review will appear</h1>
+    </div>
+  )
+}
+
+export default Writewebreview;
