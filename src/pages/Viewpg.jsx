4acabedc46ac50da6bab2f527b8b47b690749e37
praka <prakashswami150569@gmail.com>
2025-09-06 00:52:12 +0530

    added viewpg page

---
+++ src/pages/Viewpg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Viewpg = () => {
+  return (
+    <div>
+      <h1>here we will showcase information about a single hostel</h1>
+    </div>
+  )
+}
+
+export default Viewpg;
